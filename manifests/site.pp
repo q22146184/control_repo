a014d722b35e2ad0e28ee1cbab530a7151373231
q22146184 <yangwenduo02335@gmail.com>
2018-10-24 14:42:59 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,6 +2,10 @@ node default {
 }
 node 'master.puppet.vm' {
   include role::master_server
+  # Configure puppetdb and its underlying database
+  class { 'puppetdb': }
+  # Configure the Puppet master to use puppetdb
+  class { 'puppetdb::master::config': }
 }
 node /^web/ { 
   include role::app_server
